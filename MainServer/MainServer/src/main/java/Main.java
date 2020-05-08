d3b2e9ec4cb726977f2436c7993ccc7682e33b07
Aavvii <58885418+Aavvii@users.noreply.github.com>
2020-05-09 00:30:10 +0300

    Add files via upload

---
+++ MainServer/MainServer/src/main/java/Main.java
@@ -0,0 +1,25 @@
+import CommunicationUnits.MobileAppCU;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class Main {
+    public static void main(String[] args) {
+        HttpServer server = null;
+        try {
+            server = HttpServer.create(new InetSocketAddress("localhost", 6969), 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+
+        server.createContext("/test", new MobileAppCU());
+        server.createContext("/login", new LoginHandler());
+        server.setExecutor(threadPoolExecutor);
+        server.start();
+        System.out.println(" Server started on port 6969");
+    }
+}
