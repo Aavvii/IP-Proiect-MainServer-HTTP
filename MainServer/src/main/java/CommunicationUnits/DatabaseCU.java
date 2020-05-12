e419261fedd2e2c3c884d1bfb70461c6858f3969
croftlis <59141582+Elisa-byte@users.noreply.github.com>
2020-05-13 00:22:58 +0300

    errors history reviews

--- MainServer/src/main/java/CommunicationUnits/DatabaseCU.java
+++ MainServer/src/main/java/CommunicationUnits/DatabaseCU.java
@@ -42,16 +42,13 @@ public class DatabaseCU {
 
             if (out.get("password").toString().equals(in.get("password").toString())) {
                 in.put("responseCode", responseCode);
-            }
-            else in.put("responseCode", 404);
-        }
-
-        else {
+            } else in.put("responseCode", 404);
+        } else {
             in.put("responseCode", responseCode);
         }
     }
 
-    public static boolean singUpToDatabase (JSONObject in) throws IOException {
+    public static boolean singUpToDatabase(JSONObject in) throws IOException {
         URL obj = new URL("http://87.255.79.195:7532/test");
         HttpURLConnection postConnection = (HttpURLConnection) obj.openConnection();
         postConnection.setRequestMethod("POST");
@@ -70,34 +67,75 @@ public class DatabaseCU {
         postConnection.getInputStream().close();
         if (responseCode == HttpURLConnection.HTTP_OK) { //success
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
-    public static boolean requestHistory(JSONObject jsonObject) throws IOException {
-        URL obj = new URL("http://87.255.79.195:7532/test");
-        HttpURLConnection postConnection = (HttpURLConnection) obj.openConnection();
-        postConnection.setRequestMethod("POST");
-        postConnection.setRequestProperty("content-type", "application/json");
-        postConnection.setDoOutput(true);
-        JSONObject requestJson = new JSONObject();
-        requestJson.put("method", "getHistoryByUserNickname");
-        requestJson.put("argument", jsonObject.get("nickname"));//in fct de ce primesc de la mobile
-        System.out.println(requestJson.toString());
-        BufferedWriter send = new BufferedWriter(new OutputStreamWriter(postConnection.getOutputStream()));
-        send.write(requestJson.toString());
-        send.close();
+    public static JSONObject requestHistory(JSONObject jsonObject) throws IOException {
+        JSONObject jsonResponse = null;
+        try {
+            URL obj = new URL("http://87.255.79.195:7532/test");
+            HttpURLConnection postConnection = (HttpURLConnection) obj.openConnection();
+            postConnection.setRequestMethod("POST");
+            postConnection.setRequestProperty("content-type", "application/json");
+            postConnection.setDoOutput(true);
+            JSONObject requestJson = new JSONObject();
+//        { "method":"getHistoryByUserNickname",
+//                "argument":"maria123"}//DanutCiobotaru932
+            requestJson.put("method", "getHistoryByUserNickname");
+            requestJson.put("argument", jsonObject.get("username").toString());//in fct de ce primesc de la mobile
+            BufferedWriter send = new BufferedWriter(new OutputStreamWriter(postConnection.getOutputStream()));
+            send.write(requestJson.toString());
+            send.close();
 
-        int responseCode = postConnection.getResponseCode();
-        postConnection.getInputStream().close();
-        if (responseCode == HttpURLConnection.HTTP_OK) { //success
-            return true;
+            int responseCode = 0;
+            responseCode = postConnection.getResponseCode();
+            System.out.println("database history response code: " + responseCode);
+            if (responseCode == HttpURLConnection.HTTP_OK) { //success
+                BufferedReader in = null;
+                try {
+                    in = new BufferedReader(new InputStreamReader(postConnection.getInputStream()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String inputLine = null;
+                StringBuffer responseSB = new StringBuffer();
+                while (true) {
+                    if (!((inputLine = in.readLine()) != null)) break;
+                    responseSB.append(inputLine);
+                    String error = responseSB.substring(2, 7);
+                    if (error.equals("error")) {
+                        //trimitere eroare la mobile app
+                    }
+                }
+                System.out.println("response historyDB: " + responseSB);
+                if (responseSB.toString().equals("")) {
+                    jsonResponse = new JSONObject();
+                    jsonResponse.put("mesajEroare", "Eroare interna");
+                    jsonResponse.put("responseCode", "406");
+                } else {
+                    if (ErrorHandling.isValid(responseSB.toString())) {
+                        jsonResponse = new JSONObject(responseSB.toString());
+                        System.out.println(jsonResponse.toString());
+                        if (ErrorHandling.isJsonEmpty(jsonResponse, "reviews") || !jsonResponse.has("reviews")) {
+                            jsonResponse = new JSONObject();
+                            jsonResponse.put("mesajEroare", "Nu s-au gasit review-uri");
+                            jsonResponse.put("responseCode", "406");
+                        }
+                    }
+                }
+            } else {
+                jsonResponse = new JSONObject();
+                jsonResponse.put("mesajEroare", "Eroare interna");
+                jsonResponse.put("responseCode", "406");
+            }
+        } catch (
+                IOException e) {
+            e.printStackTrace();
         }
-        else { return false; }
+        return jsonResponse;
     }
-
 
     public static JSONObject databaseRequestReviews(JSONObject jsonObject) throws IOException {
 
@@ -136,27 +174,27 @@ public class DatabaseCU {
         System.out.println("  deleteBookByISBN ( 124-948-876-537-2 )");
         post(uri, post);
 */
-        if(jsonObject.has("ISBN")) {
+        if (jsonObject.has("ISBN")) {
             request.put("method", "getLatestReviewByBookID");
             // TODO: cand o sa avem metoda getReviewByBookISBN, decomenteaza urmatoarea linie
             //  request.put("argument", bookinfo.get("ISBN"));
             request.put("argument", "1");
-        }else{
-            if(jsonObject.has("reviews")){
+        } else {
+            if (jsonObject.has("reviews")) {
                 //trimitem reviews
                 //TODO facem functie aici sau separat?
             }
         }
         JSONObject JsonResponse = null;
-        postData=request.toString();
+        postData = request.toString();
         //System.out.println(postData);
         String response = post(uri, postData);
-        if(response.equalsIgnoreCase("internalError") || response.equalsIgnoreCase("noReviews")){
+        if (response.equalsIgnoreCase("internalError") || response.equalsIgnoreCase("noReviews")) {
             JsonResponse = new JSONObject("{}");
         }
         if (ErrorHandling.isValid(response)) {
             JsonResponse = new JSONObject(response);
-            if(ErrorHandling.isJsonEmpty(JsonResponse,"reviews") || !JsonResponse.has("reviews")){
+            if (ErrorHandling.isJsonEmpty(JsonResponse, "reviews") || !JsonResponse.has("reviews")) {
                 JsonResponse = new JSONObject("{}");
             }
         }
@@ -172,14 +210,14 @@ public class DatabaseCU {
                 .uri(URI.create(uri))
                 .POST(HttpRequest.BodyPublishers.ofString(data))
                 .build();
-        HttpResponse response =null;
+        HttpResponse response = null;
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            responseString=response.body().toString();
-            if(response.body().toString().substring(0,23).equalsIgnoreCase("ObjectNotFoundException")){
-                responseString ="noReviews";
+            responseString = response.body().toString();
+            if (response.body().toString().substring(0, 23).equalsIgnoreCase("ObjectNotFoundException")) {
+                responseString = "noReviews";
             }
-            if(response.body().toString().equalsIgnoreCase("Method does not exist!")){
+            if (response.body().toString().equalsIgnoreCase("Method does not exist!")) {
                 responseString = "internalError";
             }
 
@@ -191,8 +229,8 @@ public class DatabaseCU {
             e.printStackTrace();
         }
 
-        if(response == null){
-            responseString="internalError";
+        if (response == null) {
+            responseString = "internalError";
         }
         return responseString;
     }
