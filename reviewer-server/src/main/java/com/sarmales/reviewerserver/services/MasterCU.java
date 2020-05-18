0b173d3240f7722340d86e90c23e25b76ea92a68
croftlis <59141582+Elisa-byte@users.noreply.github.com>
2020-05-18 23:37:10 +0300

    json pt MA

--- reviewer-server/src/main/java/com/sarmales/reviewerserver/services/MasterCU.java
+++ reviewer-server/src/main/java/com/sarmales/reviewerserver/services/MasterCU.java
@@ -1,4 +1,5 @@
 package com.sarmales.reviewerserver.services;
+
 import com.sarmales.reviewerserver.handler.ErrorHandling;
 import org.json.JSONObject;
 
@@ -21,31 +22,37 @@ public class MasterCU {
     private String output;
     boolean flag = true;
     boolean imgProcFlag = true;
-    JSONObject errorMsg =null;
-    public void prepareResponse() {
+    boolean isbnFound = true;
+    JSONObject errorMsg = null;
+    JSONObject dbReviewsResponse = null;
+    JSONObject bookReviews = null;
+
+    public void prepareResponse() throws IOException, InterruptedException {
         JSONObject inputJSON = new JSONObject(input);
         JSONObject bookInfo = null;
+        //Caz1: Nu am trimis un isbn la mobile app : ISBN:"9780735623873" -ok
         if (inputJSON.has("isbn") && !inputJSON.get("isbn").toString().equals("")) {
+            System.out.println(" ISBN OK ... EXISTA \n \n ");
             imgProcFlag = false;
             bookInfo = new JSONObject();
             bookInfo.put("ISBN", inputJSON.get("isbn").toString());
             System.out.println(bookInfo.get("ISBN"));
-        }
-        else if (ErrorHandling.isValid(input)) {
+
+        } else if (ErrorHandling.isValid(input)) {
             System.out.println();
             // System.out.println(inputJSON.toString());
 
-            if (ErrorHandling.isJsonEmpty(inputJSON,"encoding")) {
+            if (ErrorHandling.isJsonEmpty(inputJSON, "encoding")) {
                 flag = false;
                 errorMsg = new JSONObject();
                 errorMsg.put("mesajEroare", "JSON gol");
-                errorMsg.put("responseCode","405");
+                errorMsg.put("responseCode", "405");
             }
         } else {
             errorMsg = new JSONObject();
             flag = false;
-            errorMsg.put("mesajEroare","nu este in format JSON");
-            errorMsg.put("responseCode","402");
+            errorMsg.put("mesajEroare", "nu este in format JSON");
+            errorMsg.put("responseCode", "402");
         }
 
         // TODO System.out.println(inputJSON);
@@ -53,7 +60,8 @@ public class MasterCU {
 
         JSONObject dbReviewsResponse = null;
         JSONObject bookReviews = null;
-        if(flag && imgProcFlag) {
+        if (flag && imgProcFlag) {
+            System.out.println(" FLAGS ENTER ");
             inputJSON.remove("isbn");
             bookInfo = ImageProcessorCU.requestBookInfo(inputJSON.toString());
             if (bookInfo.has("mesajEroare")) {
@@ -63,7 +71,7 @@ public class MasterCU {
             }
         }
         System.out.println(flag);
-        //System.out.println(bookInfo);
+  /*      //System.out.println(bookInfo);
 
         //input hardcodat care merge
         //JSONObject bookInfo = new JSONObject("{\"ISBN\": \"978-606-623-2\", \"author\": \"JRR Tolkien\", \"message\": \"decoded\", \"success\": \"DA\", \"title\": \"Silmarilion\"}");
@@ -74,32 +82,52 @@ public class MasterCU {
         //JSONObject bookInfo = new JSONObject("{\"ISBN\": \"978-006-092-043-2\", \"author\": \"Andy Mitchell\", \"message\": \"decoded\", \"success\": \"DA\", \"title\": \"\"}");
 
 
-        /*DBClient client = new DBClient();
+        DBClient client = new DBClient();
         try {
             DBClient.dod();
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
 
 //        System.out.println("bookInfo"+bookInfo);
-/*        try {
+       try {
             dbReviewsResponse = DatabaseCU.databaseRequestReviews(bookInfo);
             //System.out.println("aici");
         } catch (IOException e) {
             e.printStackTrace();
         }*/
-
+        JSONObject jsonObjectForMA = null;
+        boolean foundReviewError = false;
+        String bookISBN = null;
+        String errorMessage = null;
+        System.out.println("bookInfo: " + bookInfo);
         System.out.println("REVIEWS DIN DATABASE : " + dbReviewsResponse);
-        if(flag) {
+        if (flag) {
+            System.out.println("aici2");
             // TODO Decomenteaza linia urmatoare pentru a testa crawlerul
-            //dbReviewsResponse = null;
+            dbReviewsResponse = null;
             if ((dbReviewsResponse == null) || dbReviewsResponse.toString().equalsIgnoreCase("{}")) {
                 try {
                     bookReviews = ReviewCollectorCU.requestReviews(bookInfo);
-                    if(bookReviews .has("mesajEroare")){
-                        flag = false;
-                        errorMsg = new JSONObject(bookInfo.toString());
-                        output = errorMsg.toString();
+                    System.out.println("bookReviews" + bookReviews.toString());
+                    if (bookReviews.has("mesajEroare")) {
+                        foundReviewError = bookReviews.get("mesajEroare").toString().contains("review-uri");
+                        System.out.println(foundReviewError);
+                        if (foundReviewError == true) {
+                            System.out.println("aici");
+                            isbnFound = false;
+                            flag = false;
+                            bookISBN = bookInfo.get("ISBN").toString();
+                            bookISBN = bookISBN.replaceAll("[^0-9]", "");
+                            System.out.println(bookISBN);
+                            jsonObjectForMA =new JSONObject();
+                            jsonObjectForMA.put("ISBN", bookISBN);
+                            output = jsonObjectForMA.toString();
+                        } else {
+                            flag = false;
+                            errorMsg = new JSONObject(bookInfo.toString());
+                            output = errorMsg.toString();
+                        }
                     }
                     System.out.println("REVIEWS DE LA CRAWLER : " + bookReviews.toString());
 
@@ -107,14 +135,15 @@ public class MasterCU {
                 } catch (IOException | InterruptedException e) {
                     e.printStackTrace();
                 }
-            } else{
+            } else {
                 bookReviews = dbReviewsResponse;
             }
-
-            output = bookReviews.toString();
-        }
-        else
-        {
+            if (isbnFound == true) {
+                output = bookReviews.toString();
+            } else {
+                output = jsonObjectForMA.toString();
+            }
+        } else {
             output = errorMsg.toString();
         }
         System.out.println(output);
